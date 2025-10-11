adc577652482062a523eb5909128190f2a325358
Fabio <fabiolmatsuo@gmail.com>
2025-10-11 17:11:36 -0300

    ajuste exercício

---
+++ 0x07/api-rest-calculator/src/test/java/com/example/calculator/CalculatorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Testa se o contexto da aplicação Spring Boot é carregado corretamente
+    }
+}
