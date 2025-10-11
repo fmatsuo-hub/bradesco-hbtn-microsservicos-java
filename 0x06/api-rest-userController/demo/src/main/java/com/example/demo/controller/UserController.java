55eef07578fe227fc9aa8b1f607bfc9aefc2602c
Fabio <fabiolmatsuo@gmail.com>
2025-10-11 16:40:30 -0300

    Java Microsserviços - Tratamento de exceções em REST API

---
+++ 0x06/api-rest-userController/demo/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,37 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.*;
+import com.example.demo.exception.*;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+
+    @GetMapping("/user-id/{id}")
+    public String findUserById(@PathVariable int id) {
+        if (id > 0 && id < 100) {
+            return "You have entered valid ID";
+        }
+        throw new UserIdException(String.valueOf(id));
+    }
+
+    @GetMapping("/user-name/{userName}")
+    public String findUserByName(@PathVariable String userName) {
+        if (userName.length() > 3 && userName.length() < 15) {
+            return "You have entered valid USERNAME";
+        }
+        throw new UserNameException(userName);
+    }
+
+    @GetMapping("/user-cpf/{cpf}")
+    public String findUserByCPF(@PathVariable String cpf) {
+        if (isCPF(cpf)) {
+            return "You have entered valid CPF";
+        }
+        throw new CPFException(cpf);
+    }
+
+    public boolean isCPF(String cpf) {
+        return cpf.length() > 3 && cpf.length() < 15;
+    }
+}
